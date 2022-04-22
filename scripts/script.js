7cd804795df415f282ce377b3fa0306ca2e8fcdd
Matthew <Matirix@live.com>
2022-04-22 11:10:48 -0700

    initialized porject. 2 new cards. Read-mores, about-me's TBD soon.

--- scripts/script.js
+++ scripts/script.js
@@ -3,7 +3,12 @@
 //---------------------------------
 
 
-function sayHello() {
-    //do something
-}
-//sayHello();    //invoke function
+function expansion() {
+    $("#explode").html("<div class='row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3'>" + "Here" + "</div>")
+    $("#explode").css("width", "50%")
+    console.log("working")
+}   
+// function sayHello() {
+//     //do something
+// }
+// //sayHello();    //invoke function
